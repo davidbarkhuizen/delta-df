a03c9f6e42884687ee2135590b034b6526a4a7ec
david barkhuizen <david.barkhuizen@imhotep.local>
2024-04-04 13:39:31 +0200

    wip: modularised code; argparse used to create meaningful argument names; go script loops through files

--- delta.py
+++ delta.py
@@ -1,5 +1,7 @@
-# load two dataframes from file (pickle)
-# systematically compare them
+'''load two dataframes from file (pickle), and then systematically compare them'''
+
+import argparse
+from typing import List
 
 from pandas import NaT, read_pickle, DataFrame
 #import logging
@@ -16,57 +18,60 @@ def load_dataframe_from_file(file_path: str, compression: str) -> DataFrame:
         compression=compression
     )
 
-    print(f'... loaded dataframe from pkl file at {file_path}')
+    print(f'... loaded')
 
     return df
 
-
 # pandas.DataFrame.compare
 
-def compare(alpha_label: str, alpha: DataFrame, beta_label: str, beta: DataFrame) -> bool:
+def compare(
+        ref_label: str, ref_df: DataFrame, 
+        target_label: str, target_df: DataFrame
+    ) -> bool:
+    
     differences = []
 
     # row count
 
-    alpha_row_count = len(alpha.index)
-    beta_row_count = len(beta.index)
+    ref_row_count = len(ref_df.index)
+    target_row_count = len(target_df.index)
 
-    if alpha_row_count != beta_row_count:
-        differences.append(f'row counts do not match: {alpha_label} = {alpha_row_count}, {beta_label} = {beta_row_count}')
+    if ref_row_count != target_row_count:
+        differences.append(f'row counts do not match: {ref_label} = {ref_row_count}, {target_label} = {target_row_count}')
 
     # column count
 
-    alpha_col_count = len(alpha.columns.to_list())
-    beta_col_count = len(beta.columns.to_list())
+    ref_col_count = len(ref_df.columns.to_list())
+    target_col_count = len(target_df.columns.to_list())
 
-    if alpha_col_count != beta_col_count:
-        differences.append(f'column counts do not match: {alpha_label} = {alpha_col_count}, {beta_label} = {beta_col_count}')
+    if ref_col_count != target_col_count:
+        differences.append(f'column counts do not match: {ref_label} = {ref_col_count}, {target_label} = {target_col_count}')
 
     # column name
         
-    alpha_cols = alpha.columns.to_list()
-    beta_cols = beta.columns.to_list()
+    ref_cols = ref_df.columns.to_list()
+    target_cols = target_df.columns.to_list()
 
-    column_union = set(alpha_cols + beta_cols)
-    column_intersection = [col for col in column_union if col in alpha_cols and col in beta_cols]
+    column_union = set(ref_cols + target_cols)
+    column_intersection = [col for col in column_union if col in ref_cols and col in target_cols]
     column_disjoint = [col for col in column_union if col not in column_intersection]
 
     if len(column_disjoint) > 0:
         differences.append(f'disjoint columns: {"".join(column_disjoint)}')
 
-    for i in range(alpha_col_count):
-        if alpha_cols[i] != beta_cols[i]:
-            differences.append(f'column order does not match: {i} {alpha_label} {alpha_cols[i]}, {beta_label} {beta_cols[i]}')
+    for i in range(ref_col_count):
+        if ref_cols[i] != target_cols[i]:
+            differences.append(f'column order does not match: {i} {ref_label} {ref_cols[i]}, {target_label} {target_cols[i]}')
 
     # column types
 
-    for i in range(alpha_col_count):
-        alpha_dtype = alpha.dtypes.iloc[i]
-        beta_dtype = beta.dtypes.iloc[i]
-        alpha_col_name = alpha_cols[i]
-        beta_col_name = beta_cols[i]
+    for i in range(ref_col_count):
+        alpha_dtype = ref_df.dtypes.iloc[i]
+        beta_dtype = target_df.dtypes.iloc[i]
+        alpha_col_name = ref_cols[i]
+        beta_col_name = target_cols[i]
         if alpha_dtype != beta_dtype:
-            differences.append(f'column types do not match: {alpha_label} {alpha_col_name} {alpha_dtype}, {beta_label} {beta_col_name} {beta_dtype}')
+            differences.append(f'column types do not match: {ref_label} {alpha_col_name} {alpha_dtype}, {target_label} {beta_col_name} {beta_dtype}')
         #print(f'dtype for {alpha_col_name}: {alpha_dtype}')
 
     # --------------------------------------
@@ -76,16 +81,16 @@ def compare(alpha_label: str, alpha: DataFrame, beta_label: str, beta: DataFrame
 
         cols_with_val_diffs = []
 
-        for row_idx in range(alpha_row_count):
+        for row_idx in range(ref_row_count):
 
             # only log differences from the first differing row
             log_difference = value_diff_count < 1
             row_has_difference = False 
 
-            for col_idx in range(alpha_col_count):
-                col = alpha_cols[col_idx]
-                alpha_val = alpha[col].iloc[row_idx]
-                beta_val = beta[col].iloc[row_idx]
+            for col_idx in range(ref_col_count):
+                col = ref_cols[col_idx]
+                alpha_val = ref_df[col].iloc[row_idx]
+                beta_val = target_df[col].iloc[row_idx]
                 if alpha_val != beta_val:
                     if not pandas.isnull(alpha_val) and not pandas.isnull(beta_val):
                         row_has_difference = True
@@ -93,23 +98,23 @@ def compare(alpha_label: str, alpha: DataFrame, beta_label: str, beta: DataFrame
                         if col not in cols_with_val_diffs: cols_with_val_diffs.append(col)
                         
                         if log_difference:
-                            text = f'value difference in row {row_idx} column {col}: {alpha_label} ({type(alpha_val)}) : {alpha_val} compare {beta_label} ({type(beta_val)}) : {beta_val}'
+                            text = f'value difference in row {row_idx} column {col}: {ref_label} ({type(alpha_val)}) : {alpha_val} compare {target_label} ({type(beta_val)}) : {beta_val}'
                             differences.append(text)
 
             if row_has_difference and log_difference:
                 print('=-'*40)
-                print(f'{alpha_label}')
+                print(f'{ref_label}')
                 print()
-                print(f'{alpha.iloc[row_idx]}')
+                print(f'{ref_df.iloc[row_idx]}')
 
                 print('=-'*40)
-                print(f'{beta_label}')
+                print(f'{target_label}')
                 print()
-                print(f'{beta.iloc[row_idx]}')
+                print(f'{target_df.iloc[row_idx]}')
 
 
         if value_diff_count > 0:
-            differences.append(f'found {value_diff_count} value differences in {alpha_row_count} records, across columns: {",".join(cols_with_val_diffs)}')
+            differences.append(f'found {value_diff_count} value differences in {ref_row_count} records, across columns: {",".join(cols_with_val_diffs)}')
 
     value_delta()
    
@@ -122,40 +127,30 @@ def compare(alpha_label: str, alpha: DataFrame, beta_label: str, beta: DataFrame
     return identical
 
 def diff(
-        alpha_label: str, alpha_path: str, 
-        beta_label: str, beta_path: str,
-        in_place_remediations: Callable[[DataFrame], None],
+        ref_label: str, ref_path: str, ref_compression: str,
+        target_label: str, target_path: str, target_compression: str,
+        sort_order: List[str],
+        in_place_remediations: Callable[[DataFrame], None] = None
     ):
 
-    print('loading from file ...')
+    ref_df = load_dataframe_from_file(ref_path, compression=ref_compression) # zip
+    target_df = load_dataframe_from_file(target_path, compression=target_compression) # # gzip
 
-    alpha_df = load_dataframe_from_file(alpha_path, compression='zip')
-    beta_df = load_dataframe_from_file(beta_path, compression='gzip')
-
-    if in_place_remediations is not None:
-        print(f'applying in-place remediations to {alpha_label} ...')
-        in_place_remediations(alpha_df)
+    # remediations
+    if in_place_remediations:
+        in_place_remediations(target_df)
 
     # align
 
-    sort_order = [ 
-        'main_start_date', 
-        'kk_date', 
-        'trip_id', 
-        'start_date', 
-        'dossier_code', 
-        'actual_trip_code', 
-        'core_trip_code' 
-    ] 
-    
-    for df in [alpha_df, beta_df]:
+    print(f'dataframes sorted in order: {", ".join(sort_order)}')
+    for df in [ref_df, target_df]:
         df.sort_values(by=sort_order, inplace=True)
 
     # compare
         
     print('comparing ...')
         
-    identical = compare(alpha_label, alpha_df, beta_label, beta_df)
+    identical = compare(ref_label, ref_df, target_label, target_df)
     if identical:
         print('data frames are identical')
     else:
@@ -163,20 +158,36 @@ def diff(
 
     return identical
 
-args = sys.argv[1:]
+# ---------
+REF_PATH = 'ref_path'
+REF_LABEL = 'ref_label'
+REF_COMPRESSION = 'ref_compression'
 
-alpha_label = args[0] 
-alpha_path = args[1]
+TARGET_PATH = 'target_path'
+TARGET_LABEL = 'target_label'
+TARGET_COMPRESSION = 'target_compression'
 
-beta_label = args[2]
-beta_path = args[3]
+SORT_ORDER = 'sort_order'
 
-print(f'comparing {alpha_label} and {beta_label}')
-print(f'{alpha_label} @ {alpha_path}')
-print(f'{beta_label} @ {beta_path}')
+def parse_args():
+    parser = argparse.ArgumentParser(description='produce a readable summary of the delta of two pandas dataframes')
 
-def in_place_remediations_to_alpha(alpha_df: DataFrame):
-    alpha_df.replace('', None, inplace=True)
-    #pass
+    parser.add_argument(f'--{REF_PATH}', help='file path to reference df', required=True)
+    parser.add_argument(f'--{REF_COMPRESSION}', help='compression used when pickling reference DF', required=True)
+    parser.add_argument(f'--{REF_LABEL}', help='label of reference df', default='reference', required=False)
 
-print(diff(alpha_label, alpha_path, beta_label, beta_path, in_place_remediations_to_alpha))
+    parser.add_argument(f'--{TARGET_PATH}', help='file path to target df to compare against reference', required=True)
+    parser.add_argument(f'--{TARGET_COMPRESSION}', help='compression used when pickling target DF', required=True)
+    parser.add_argument(f'--{TARGET_LABEL}', help='label of target df', default='target', required=False)
+
+    parser.add_argument(f'--{SORT_ORDER}', help='csv list of column names in which order to sort dfs before comparison', required=True)
+
+    return vars(parser.parse_args())
+
+args = parse_args()
+identical = diff(
+    args[REF_LABEL], args[REF_PATH], args[REF_COMPRESSION],
+    args[TARGET_LABEL], args[TARGET_PATH], args[TARGET_COMPRESSION],
+    sort_order = args[SORT_ORDER].split(','),
+    in_place_remediations=lambda df: df.replace('', None, inplace=True)
+)
--- go.sh
+++ go.sh
@@ -2,10 +2,22 @@
 
 . .venv/bin/activate
 
-python3 delta.py \
-    'local-dev' '/Users/david.barkhuizen/development/lumos/mount/celery-worker-heavy/etl_input.pkl' \
-    'on-prem' '/Users/david.barkhuizen/code/delta-df/in/on-prem/etl_input.pkl'
+declare -a file_names=(
+    "etl_input.pkl"
+    "before_determining_bookings.pkl"
+)
 
-python3 delta.py \
-    'local-dev' '/Users/david.barkhuizen/development/lumos/mount/celery-worker-heavy/before_determining_bookings.pkl' \
-    'on-prem' '/Users/david.barkhuizen/code/delta-df/in/on-prem/before_determining_bookings.pkl'
+sort_order="main_start_date,kk_date,trip_id,start_date,dossier_code,actual_trip_code,core_trip_code"
+
+for file_name in "${file_names[@]}"
+do
+    echo "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-"
+    python3 delta.py \
+        --ref_label='on-prem' \
+        --ref_path="/Users/david.barkhuizen/code/delta-df/in/on-prem/${file_name}" \
+        --ref_compression='gzip' \
+        --target_label='local-dev' \
+        --target_path="/Users/david.barkhuizen/development/lumos/mount/celery-worker-heavy/${file_name}" \
+        --target_compression='zip' \
+        --sort_order="${sort_order}"
+done
